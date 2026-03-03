35281d64958b282202e0dd79a3e96b1d7ceb027e
Estiak Ahmed <estiakahmed898@gmail.com>
2026-03-03 10:03:57 +0600

    Layout Issues fixed

--- app/kitabghor/layout.tsx
+++ app/kitabghor/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import Footer from "@/components/ecommarce/footer";
 import Header from "@/components/ecommarce/header";
 import { useEffect, useState } from "react";
@@ -17,7 +18,9 @@ const KitabGhorLayout = ({ children }: { children: React.ReactNode }) => {
         <div className="min-h-screen flex flex-col">
           {/* Header placeholder to maintain layout structure */}
           <div className="h-20 bg-background border-b border-border"></div>
-          {children}
+          <div key="layout-children-loading">
+            {children}
+          </div>
           {/* Footer placeholder to maintain layout structure */}
           <div className="h-96 bg-card border-t border-border"></div>
         </div>
@@ -29,7 +32,9 @@ const KitabGhorLayout = ({ children }: { children: React.ReactNode }) => {
     <div className="min-h-full">
       <div className="min-h-screen flex flex-col">
         <Header />
-        {children}
+        <div key="layout-children">
+          {children}
+        </div>
         <Footer />
       </div>
     </div>
