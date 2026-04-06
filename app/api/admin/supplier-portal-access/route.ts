db77f5f50f4dd765862ec9cf60ae3a0370a8d676
BoedZisan <stack1@birdsofeden.me>
2026-04-06 16:32:24 +0600

    supplier portal access updated !

--- app/api/admin/supplier-portal-access/route.ts
+++ app/api/admin/supplier-portal-access/route.ts
@@ -71,7 +71,7 @@ export async function GET(request: NextRequest) {
       }),
       prisma.user.findMany({
         where: {
-          email: { not: null },
+          email: { not: "" },
           ...(search
             ? {
                 OR: [
